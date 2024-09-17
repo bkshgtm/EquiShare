026c1bcef4b58e3910ed9ed29162b1fec5a406ad
Bikash Gautam <bikash.gautam098@gmail.com>
2024-09-17 14:57:04 -0500

    Initial Commit

---
+++ EquiShare/EquiShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  EquiShareApp.swift
+//  EquiShare
+//
+//  Created by BkshMac on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct EquiShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
